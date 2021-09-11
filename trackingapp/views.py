7a15015f0d9d120769db013093451d552080487c
abhijithmadu <abhimnair3@gmail.com>
2021-09-11 22:00:41 +0530

    Final code for tracking error calculation

--- trackingapp/views.py
+++ trackingapp/views.py
@@ -1,37 +1,37 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
 import requests
-
-# Create your views here.
+from decouple import config
 
 def sample(request):
-    key = 'd2f6235ac39c5e90926f6c12ac5d9209'
+    #API access key
+    key =config('secret_key')
     params = {
         'access_key': key
             }
 
+    
+    #collect sbi etf values
     present_etf = requests.get('https://api.marketstack.com/v1/tickers/SETFNN50.XNSE/eod/latest',params)
     sbi_present_etf=present_etf.json()['close']
-    print(sbi_present_etf)
-
+    
     previous_etf = requests.get('http://api.marketstack.com/v1/tickers/SETFNN50.XNSE/eod/2021-03-09',params)
     sbi_previous_etf=previous_etf.json()['close']
-    print(sbi_previous_etf)
-
+    
+    #collect sbi nifty values
     present_nifty = requests.get('https://api.marketstack.com/v1/tickers/NN50.INDX/eod/latest',params)
     sbi_present_nifty = present_nifty.json()['close']
-    print(sbi_present_nifty)
     
     previous_nifty = requests.get('http://api.marketstack.com/v1/tickers/NN50.INDX/eod/2021-03-09', params)
     sbi_previous_nifty = previous_nifty.json()['close']
-    print(sbi_previous_nifty)
+    
 
-
+    #tracking error calculation
     sbi_diff = sbi_present_etf/sbi_previous_etf-1
     nifty_diff = sbi_present_nifty/sbi_previous_nifty-1
 
     tracking_error = abs((sbi_diff-nifty_diff)*100)
-    print(tracking_error)
+    
     context = {
         'tracking_error':tracking_error
     }
